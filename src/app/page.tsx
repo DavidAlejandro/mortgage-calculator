698594239dcc8b63da07f86e5c7b5ac5357f6cd2
David Porcel <davidporcel@Davids-MacBook-Pro.local>
2024-12-08 06:16:25 -0600

    style: add min height, min width and padding to the page main div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { MortgageCalculator } from "./components/MortgageCalculator/MortgageCalc
 
 export default function Home() {
   return (
-    <div className="mx-auto flex h-screen w-1/2 flex-col items-center justify-center">
+    <div className="mx-auto flex h-screen min-h-fit min-w-fit flex-col items-center justify-center p-3">
       <MortgageCalculator />
     </div>
   );
